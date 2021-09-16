fa5fe6a615e3d374417af650ce03ff7e209478aa
Trayvon001 <q447957635@icloud.com>
2021-09-16 22:31:12 +0800

    测试自动更新

--- moc_bordercollie.cpp
+++ moc_bordercollie.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_bordercollie_t {
-    QByteArrayData data[10];
-    char stringdata0[146];
+    QByteArrayData data[14];
+    char stringdata0[209];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -37,18 +37,24 @@ QT_MOC_LITERAL(1, 13, 10), // "mouseMove1"
 QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 12), // "QMouseEvent*"
 QT_MOC_LITERAL(4, 38, 5), // "event"
-QT_MOC_LITERAL(5, 44, 21), // "on_pushButton_clicked"
-QT_MOC_LITERAL(6, 66, 22), // "on_addData_btn_clicked"
-QT_MOC_LITERAL(7, 89, 27), // "on_tabWidget_currentChanged"
-QT_MOC_LITERAL(8, 117, 5), // "index"
-QT_MOC_LITERAL(9, 123, 22) // "on_display_btn_clicked"
+QT_MOC_LITERAL(5, 44, 13), // "replyFinished"
+QT_MOC_LITERAL(6, 58, 14), // "QNetworkReply*"
+QT_MOC_LITERAL(7, 73, 6), // "reply1"
+QT_MOC_LITERAL(8, 80, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(9, 102, 22), // "on_addData_btn_clicked"
+QT_MOC_LITERAL(10, 125, 27), // "on_tabWidget_currentChanged"
+QT_MOC_LITERAL(11, 153, 5), // "index"
+QT_MOC_LITERAL(12, 159, 22), // "on_display_btn_clicked"
+QT_MOC_LITERAL(13, 182, 26) // "on_checkUpdate_btn_clicked"
 
     },
     "bordercollie\0mouseMove1\0\0QMouseEvent*\0"
-    "event\0on_pushButton_clicked\0"
+    "event\0replyFinished\0QNetworkReply*\0"
+    "reply1\0on_pushButton_clicked\0"
     "on_addData_btn_clicked\0"
     "on_tabWidget_currentChanged\0index\0"
-    "on_display_btn_clicked"
+    "on_display_btn_clicked\0"
+    "on_checkUpdate_btn_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -58,7 +64,7 @@ static const uint qt_meta_data_bordercollie[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       5,   14, // methods
+       7,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -66,17 +72,21 @@ static const uint qt_meta_data_bordercollie[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   39,    2, 0x0a /* Public */,
-       5,    0,   42,    2, 0x08 /* Private */,
-       6,    0,   43,    2, 0x08 /* Private */,
-       7,    1,   44,    2, 0x08 /* Private */,
-       9,    0,   47,    2, 0x08 /* Private */,
+       1,    1,   49,    2, 0x0a /* Public */,
+       5,    1,   52,    2, 0x0a /* Public */,
+       8,    0,   55,    2, 0x08 /* Private */,
+       9,    0,   56,    2, 0x08 /* Private */,
+      10,    1,   57,    2, 0x08 /* Private */,
+      12,    0,   60,    2, 0x08 /* Private */,
+      13,    0,   61,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
+    QMetaType::Void, 0x80000000 | 6,    7,
     QMetaType::Void,
     QMetaType::Void,
-    QMetaType::Void, QMetaType::Int,    8,
+    QMetaType::Void, QMetaType::Int,   11,
+    QMetaType::Void,
     QMetaType::Void,
 
        0        // eod
@@ -89,11 +99,24 @@ void bordercollie::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->mouseMove1((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
-        case 1: _t->on_pushButton_clicked(); break;
-        case 2: _t->on_addData_btn_clicked(); break;
-        case 3: _t->on_tabWidget_currentChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
-        case 4: _t->on_display_btn_clicked(); break;
+        case 1: _t->replyFinished((*reinterpret_cast< QNetworkReply*(*)>(_a[1]))); break;
+        case 2: _t->on_pushButton_clicked(); break;
+        case 3: _t->on_addData_btn_clicked(); break;
+        case 4: _t->on_tabWidget_currentChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->on_display_btn_clicked(); break;
+        case 6: _t->on_checkUpdate_btn_clicked(); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 1:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 0:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QNetworkReply* >(); break;
+            }
+            break;
         }
     }
 }
@@ -127,13 +150,13 @@ int bordercollie::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 5)
+        if (_id < 7)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 5;
+        _id -= 7;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 5)
-            *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 5;
+        if (_id < 7)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 7;
     }
     return _id;
 }
