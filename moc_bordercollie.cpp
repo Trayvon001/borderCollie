f536bad274c84713c1299fa819e060d5feb9c2b6
Trayvon001 <q447957635@icloud.com>
2021-10-11 21:01:29 +0800

    上传网速计

--- moc_bordercollie.cpp
+++ moc_bordercollie.cpp
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_bordercollie_t {
-    QByteArrayData data[21];
-    char stringdata0[332];
+    QByteArrayData data[24];
+    char stringdata0[400];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -52,7 +52,10 @@ QT_MOC_LITERAL(16, 242, 16), // "displayUpdateLog"
 QT_MOC_LITERAL(17, 259, 22), // "on_connect_btn_clicked"
 QT_MOC_LITERAL(18, 282, 16), // "socket_Read_Data"
 QT_MOC_LITERAL(19, 299, 19), // "socket_Disconnected"
-QT_MOC_LITERAL(20, 319, 12) // "wifiReadData"
+QT_MOC_LITERAL(20, 319, 12), // "wifiReadData"
+QT_MOC_LITERAL(21, 332, 20), // "on_clear_btn_clicked"
+QT_MOC_LITERAL(22, 353, 19), // "on_send_btn_clicked"
+QT_MOC_LITERAL(23, 373, 26) // "on_stopDisplay_btn_clicked"
 
     },
     "bordercollie\0mouseMove1\0\0QMouseEvent*\0"
@@ -63,7 +66,9 @@ QT_MOC_LITERAL(20, 319, 12) // "wifiReadData"
     "on_display_btn_clicked\0"
     "on_checkUpdate_btn_clicked\0displayUpdateLog\0"
     "on_connect_btn_clicked\0socket_Read_Data\0"
-    "socket_Disconnected\0wifiReadData"
+    "socket_Disconnected\0wifiReadData\0"
+    "on_clear_btn_clicked\0on_send_btn_clicked\0"
+    "on_stopDisplay_btn_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -73,7 +78,7 @@ static const uint qt_meta_data_bordercollie[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-      13,   14, // methods
+      16,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -81,19 +86,22 @@ static const uint qt_meta_data_bordercollie[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   79,    2, 0x0a /* Public */,
-       5,    1,   82,    2, 0x0a /* Public */,
-       8,    1,   85,    2, 0x0a /* Public */,
-      10,    0,   88,    2, 0x08 /* Private */,
-      11,    0,   89,    2, 0x08 /* Private */,
-      12,    1,   90,    2, 0x08 /* Private */,
-      14,    0,   93,    2, 0x08 /* Private */,
-      15,    0,   94,    2, 0x08 /* Private */,
-      16,    0,   95,    2, 0x08 /* Private */,
-      17,    0,   96,    2, 0x08 /* Private */,
-      18,    0,   97,    2, 0x08 /* Private */,
-      19,    0,   98,    2, 0x08 /* Private */,
-      20,    0,   99,    2, 0x08 /* Private */,
+       1,    1,   94,    2, 0x0a /* Public */,
+       5,    1,   97,    2, 0x0a /* Public */,
+       8,    1,  100,    2, 0x0a /* Public */,
+      10,    0,  103,    2, 0x08 /* Private */,
+      11,    0,  104,    2, 0x08 /* Private */,
+      12,    1,  105,    2, 0x08 /* Private */,
+      14,    0,  108,    2, 0x08 /* Private */,
+      15,    0,  109,    2, 0x08 /* Private */,
+      16,    0,  110,    2, 0x08 /* Private */,
+      17,    0,  111,    2, 0x08 /* Private */,
+      18,    0,  112,    2, 0x08 /* Private */,
+      19,    0,  113,    2, 0x08 /* Private */,
+      20,    0,  114,    2, 0x08 /* Private */,
+      21,    0,  115,    2, 0x08 /* Private */,
+      22,    0,  116,    2, 0x08 /* Private */,
+      23,    0,  117,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
@@ -102,6 +110,9 @@ static const uint qt_meta_data_bordercollie[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -132,6 +143,9 @@ void bordercollie::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id
         case 10: _t->socket_Read_Data(); break;
         case 11: _t->socket_Disconnected(); break;
         case 12: _t->wifiReadData(); break;
+        case 13: _t->on_clear_btn_clicked(); break;
+        case 14: _t->on_send_btn_clicked(); break;
+        case 15: _t->on_stopDisplay_btn_clicked(); break;
         default: ;
         }
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
@@ -177,13 +191,13 @@ int bordercollie::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 13)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 13;
+        _id -= 16;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 13)
+        if (_id < 16)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 13;
+        _id -= 16;
     }
     return _id;
 }
